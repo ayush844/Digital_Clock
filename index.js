c99bba2693d076f926e80a06a9bceed10f844302
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2023-03-29 06:45:23 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+const hourEl = document.getElementById("hour");
+
+const minEl = document.getElementById("minutes");
+
+const secEl = document.getElementById("second");
+
+const ampmEl = document.getElementById("ampm");
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ap = "AM";
+    
+    if(h > 12){
+        h = h - 12;
+        ap = "PM";
+    }
+
+    h = (h < 10)? "0" + h : h;
+    m = (m < 10)? "0" + m : m;
+    s = (s < 10)? "0" + s : s;
+
+    hourEl.innerText = h;
+    minEl.innerText = m;
+    secEl.innerText = s;
+    ampmEl.innerText = ap;
+
+    setTimeout(() => {
+        updateClock()
+    },1000);
+}
+
+updateClock();
